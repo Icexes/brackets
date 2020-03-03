d6f1a2e683260571db2c0f316ab552ddf504638d
Denis <suboch.denny@gmail.com>
2020-03-03 16:20:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+ module.exports = 
+function check(str, bracketsConfig) {
+  let config = new Map(bracketsConfig);
+  let result = str.split("");
+  for (let i=0;i<result.length;i++) {
+    if (config.get(result[i])) {
+
+      if (result[i+1]==config.get(result[i])) {
+        result.splice(i,2)
+        i-=2
+      }
+    }
+  }
+  return result.length==0 ?  true : false;
 }
